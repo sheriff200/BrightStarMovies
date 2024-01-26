e43d3d39b25bbf159ebc66147eccf03bd62e87cd
ebelebe sheriff <sheriff.ebelebe@gmail.com>
2024-01-26 16:14:58 +0100

    Implemented interface for movie

---
+++ Movies.Domain/Models/WebApiResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Domain.Models
+{
+    public class WebApiResponse
+    {
+        public string StatusCode { get; set; }
+        public string ResponseCode { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
--- Movies/Controllers/MovieController.cs
+++ Movies/Controllers/MovieController.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Movies.Domain.Models;
 
 namespace Movies.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/movie")]
     [ApiController]
     public class MovieController : ControllerBase
     {
+        [HttpPost]
+        [Route("create-movie")]
+        public IActionResult CreateMovie([FromBody] Movie model)
+        {
+
+        }
     }
 }
