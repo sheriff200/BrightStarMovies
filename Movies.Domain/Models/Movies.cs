a6d88eaad27ab6b3621f5dcb033b3e9332ee1e9a
ebelebe sheriff <sheriff.ebelebe@gmail.com>
2024-01-26 15:09:16 +0100

    --Created controller for movie and genre

---
+++ Movies.Domain/Models/Movies.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Domain.Models
+{
+    public class Movies
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleasedDate { get; set; }
+        public MovieRating Rating { get; set; }
+        public decimal TicketPrice { get; set; }
+        public string Country { get; set; }
+        public long GenreId { get; set; }
+        public string Photo { get; set; }
+    }
+
+    public enum MovieRating
+    {
+        first = 1,
+        second = 2,
+        third = 3,
+        forth = 4,
+        fivth = 5
+    }
+}
